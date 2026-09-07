16a49582fdc06126cfd89f04e0b7b9ee0abab1fb
Rashmi M <kudlarashmi@gmail.com>
2026-09-07 21:33:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (33.69%) - LeetSync

---
+++ 1086-divisor-game/divisor-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        boolean ans=false;
+        if(n%2==0)
+        {
+            ans=true;
+        }
+        return ans;
+    }
+}
