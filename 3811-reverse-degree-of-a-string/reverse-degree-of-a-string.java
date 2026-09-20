e5ff45afa1b1c508219fa86fa5dac4395a363416
Rashmi M <kudlarashmi@gmail.com>
2026-09-20 10:08:31 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (34.29%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int ans=0;
+        for(int i=0; i<s.length();i++)
+        {
+            ans=ans+(26-(s.charAt(i)-'a'))*(i+1);
+        }
+        return ans;
+    }
+}
